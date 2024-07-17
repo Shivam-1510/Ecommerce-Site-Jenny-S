425db3caa6e7a0778ca38b13c8e1fc0874595fbc
Shivam-1510 <shivam.bt22cse@pec.edu.in>
2024-07-17 22:51:56 +0530

    Ecommerce-Site-jenny-S

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ if(close){
         nav.classList.remove('active');
     })
 }
+
+const filterButtons = document.querySelectorAll('.filter-buttons button')
+const fiterableCards = document.querySelectorAll('.fiterable-cards .card')
+
+
+
+const filterCards = e => {
+    document.querySelector(".active")?.classList.remove("active")
+    e.target.classList.add("active")
+    fiterableCards.forEach(card => {
+        card.classList.add('hide')
+        if (card.dataset.name === e.target.dataset.name || e.target.dataset.name == 'all') {
+            card.classList.remove('hide')
+        }
+    })
+
+
+}
+
+const addHref = e => {
+    e.innerHtml('a')
+}
+
+
+
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards))
